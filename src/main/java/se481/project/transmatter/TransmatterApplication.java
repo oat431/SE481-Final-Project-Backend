210514331c5a78ba7b88d1d15aa73e2fd5e66459
oat431 <oat431@gmail.com>
2022-02-27 21:39:05 +0700

    [🛠 setup] : IR project

---
+++ src/main/java/se481/project/transmatter/TransmatterApplication.java
@@ -0,0 +1,13 @@
+package se481.project.transmatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransmatterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransmatterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se481/project/transmatter/TransmatterApplicationTests.java
@@ -0,0 +1,13 @@
+package se481.project.transmatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransmatterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
